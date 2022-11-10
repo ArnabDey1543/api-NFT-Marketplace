06138ffe530ccba34127befac4527345e05793a7
ArnabDey1543 <arnabdey1543@gmail.com>
2022-11-11 03:32:17 +0530

    nft model

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
-dotenv.config({path: "./config.env"});
+dotenv.config({ path: "./config.env" });
 
 
 const DB = process.env.DATABASE.replace(
@@ -24,20 +24,36 @@ mongoose.connect(DB, {
 // console.log(process.env);
 
 const nftSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: [true, "A NFT must have a name"],
         unique: true,
     },
+    description: String,
+    traitType: String,
     price: Number,
 });
 
-const NFT = mongoose.model("NFT",nftSchema);
+const NFT = mongoose.model("NFT", nftSchema);
+
+const testNFT = new NFT({
+    name: "The Insane Monkey",
+    description: "abstract",
+    traitType: "xyz",
+    price: 657
+})
+
+testNFT.save().then(docNFT => {
+    console.log(docNFT);
+}).catch(error => {
+    console.log("ERROR:", error);
+});
+
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
+    console.log(`App running on port ${port}....`);
 });
 
 
